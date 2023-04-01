cd63e6482c7647a4ac0628c0afe475af6e66dc2e
Takumi Matsuura <tm99hjkl@gmail.com>
2023-04-01 19:53:44 +0900

    feat(wezterm): change key bindings of change window pane

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,14 +1,37 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 return {
   color_scheme = "Catppuccin Mocha",
-  default_prog = {"wsl.exe"},
-  window_background_opacity = 0.9;
+  default_prog = { "wsl.exe" },
+  window_background_opacity = 0.9,
   window_padding = {
     left = 2,
     right = 2,
     top = 4,
     bottom = 0,
   },
-  font = wezterm.font("Source Code Pro", {weight=400, stretch="Normal", italic=false}),
+  font = wezterm.font("Source Code Pro", { weight = 400, stretch = "Normal", italic = false }),
   font_size = 10,
+  keys = {
+    {
+      key = 'h',
+      mods = 'ALT',
+      action = act.ActivatePaneDirection 'Left',
+    },
+    {
+      key = 'l',
+      mods = 'ALT',
+      action = act.ActivatePaneDirection 'Right',
+    },
+    {
+      key = 'k',
+      mods = 'ALT',
+      action = act.ActivatePaneDirection 'Up',
+    },
+    {
+      key = 'j',
+      mods = 'ALT',
+      action = act.ActivatePaneDirection 'Down',
+    },
+  }
 }
