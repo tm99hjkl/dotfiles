e9d6fecd51fbe19cb435d66c8cb69b56e8beda4f
Takumi Matsuura <tm99hjkl@gmail.com>
2023-10-07 20:43:23 +0900

    wezterm: change some preference settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,21 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 return {
-  color_scheme = "Gruvbox dark, hard (base16)",
+  -- color_scheme = "Gruvbox dark, hard (base16)",
+  -- color_scheme = "neobones_light",
+  color_scheme = "nord",
+
   default_prog = { "wsl.exe" },
+
   window_background_opacity = 0.9,
   window_padding = {
     left = 2,
     right = 2,
-    top = 4,
+    top = 0,
     bottom = 0,
   },
-  font = wezterm.font("Source Code Pro", { weight = 400, stretch = "Normal", italic = false }),
+
+  font = wezterm.font("Source Code Pro", { weight = 'Regular', stretch = "Normal", italic = false }),
   font_size = 10,
 
   -- Activate Pane Direction
@@ -36,7 +41,7 @@ return {
       action = act.ActivatePaneDirection 'Down',
     },
 
-  -- Adjust Pane Size
+    -- Adjust Pane Size
     {
       key = 'h',
       mods = 'ALT|CTRL',
