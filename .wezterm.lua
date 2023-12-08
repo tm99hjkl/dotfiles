b71e18b7e3f8646eaef513ea4f8ba2eec25d4220
Takumi Matsuura <tm99hjkl@gmail.com>
2023-12-08 19:48:05 +0900

    helix, wezterm: refactor

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,25 +1,30 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
+
 return {
-  -- color_scheme = "Gruvbox dark, hard (base16)",
-  -- color_scheme = "neobones_light",
-  color_scheme = "nord",
+  default_prog = { 'wsl.exe' },
 
-  default_prog = { "wsl.exe" },
+  color_scheme = 'nord',
 
-  window_background_opacity = 0.9,
   window_padding = {
-    left = 2,
-    right = 2,
+    left = 0,
+    right = 0,
     top = 0,
     bottom = 0,
   },
 
-  font = wezterm.font("Source Code Pro", { weight = 'Regular', stretch = "Normal", italic = false }),
-  font_size = 10,
+  font = wezterm.font(
+    'Source Code Pro',
+    {
+      weight = 'Regular',
+      stretch = 'Normal',
+      italic = false
+    }
+  ),
+  font_size = 14,
 
-  -- Activate Pane Direction
   keys = {
+    -- Activate Pane Direction
     {
       key = 'h',
       mods = 'ALT',
