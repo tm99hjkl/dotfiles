e58be515b3ab2fa214979b0141e7097ec430533a
Takumi Matsuura <tm99hjkl@gmail.com>
2024-02-13 15:41:23 +0900

    setup: clarify the code of installation

--- setup.sh
+++ setup.sh
@@ -1,30 +1,77 @@
-# install go
- curl -LO https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
- sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
- echo
- 
- # install rust
- curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
- source "$HOME/.cargo/env"
- 
- # install helix
- sudo add-apt-repository ppa:maveonair/helix-editor
- sudo apt update
- sudo apt install helix
- 
- # install ghq
- go isntall github.com/x-motemen/ghq@latest
- 
- # install dotfiles
- ghq get https://github.com/tm99hjkl/dotfiles
- 
- # install bat
- sudo apt install bat
- mkdir -p ~/.local/bin
- ln -s /usr/bin/batcat ~/.local/bin/bat
- 
- # install node
- curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
- export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
- [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
- nvm install node
+#!/usr/bin/env bash
+
+# Do not change the order.
+WILL_BE_INSTALLED=(
+	curl
+	git
+	go
+	ghq
+	lg
+	cargo
+	hx
+	bat
+	fd
+	rg
+)	
+
+#
+# helper functions
+#
+install_curl() {
+	sudo apt install curl
+}
+
+install_git() {
+	sudo apt install git
+}
+
+install_go() {
+	curl -LO https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
+	sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
+}
+
+install_ghq() {
+	go isntall github.com/x-motemen/ghq@latest
+}
+
+install_lg() {
+	go install github.com/jesseduffield/lazygit@latest
+	path="$(command -v lazygit)"
+	[[ -n $path ]] && ln -s $path $(dirname $path)/lg
+}
+
+install_cargo() {
+	curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+	source "$HOME/.cargo/env"
+}
+
+install_hx() {
+	sudo add-apt-repository ppa:maveonair/helix-editor
+	sudo apt install helix
+}
+
+install_bat() {
+	sudo apt install bat
+	ln -s /usr/bin/batcat /usr/bin/bat
+}
+
+install_fd() {
+	sudo apt install fd-find
+	ln -s /usr/bin/fdfind /usr/bin/fd
+}
+
+install_rg() {
+	cargo install ripgrep
+}
+
+check_and_install() {	
+	[[ -z `command -v $1` ]] && echo "[+] Starting install $1..." && install_$1
+}
+
+#
+# main
+#
+sudo apt update
+for cmd in ${WILL_BE_INSTALLED[@]}; do
+	check_and_install $cmd
+done
